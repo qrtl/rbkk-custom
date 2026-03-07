156b33b77a52eee6f6d8f5b2b4fc4a9343f57313
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2026-03-07 19:45:23 +0900

    [6195][ADD] product_chemical (#2)

---
+++ product_chemical/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2026 Quartile (https://www.quartile.co)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "Product Chemical",
+    "version": "18.0.1.0.0",
+    "category": "Inventory/Inventory",
+    "summary": "Add chemical flag to products",
+    "author": "Quartile",
+    "website": "https://www.quartile.co",
+    "license": "LGPL-3",
+    "depends": ["product"],
+    "data": [
+        "views/product_template_views.xml",
+    ],
+    "installable": True,
+}
