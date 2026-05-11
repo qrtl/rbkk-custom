59be623b74ccc9c08ce5c76eae37d6b886f1f358
Shinnosuke Morita <m.shinnosuke1202@gmail.com>
2026-05-11 15:59:13 +0900

    [6651][ADD] purchase_security_adjust
    [6651][ADD] purchase_security_adjust

---
+++ purchase_security_adjust/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2026 Quartile (https://www.quartile.co)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "Purchase Security Adjust",
+    "version": "18.0.1.0.0",
+    "category": "Purchase",
+    "summary": "Allow product managers to access purchase-related fields on products",
+    "author": "Quartile",
+    "website": "https://www.quartile.co",
+    "license": "LGPL-3",
+    "depends": ["purchase"],
+    "data": [
+        "views/product_views.xml",
+    ],
+    "installable": True,
+}
---
+++ purchase_security_adjust/readme/DESCRIPTION.md
@@ -0,0 +1,8 @@
+This module adjusts purchase-related security settings to better fit the client's
+operational needs.
+
+Changes:
+
+- Product form: Make the "Purchase" tab and the "Vendor Bills" group visible to product
+  managers, so they can manage purchase-related fields without requiring a purchase
+  role.
