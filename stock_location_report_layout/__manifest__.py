d505c09db81deba4fd6e35ccbd2a2098a8f6c9cc
nobuQuartile <nobu@quartile.co>
2026-06-11 05:30:10 +0000

    [6884][ADD] stock_location_report_layout

---
+++ stock_location_report_layout/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2026 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Location Report Layout",
+    "version": "18.0.1.0.0",
+    "category": "Inventory/Inventory",
+    "summary": "Add a readonly manager layout option to the locations report",
+    "author": "Quartile",
+    "website": "https://www.quartile.co",
+    "license": "AGPL-3",
+    "maintainer": "Toshikimi Shigenobu",
+    "depends": ["stock"],
+    "data": [
+        "security/stock_location_report_layout_security.xml",
+        "views/stock_quant_views.xml",
+    ],
+    "installable": True,
+}
---
+++ stock_location_report_layout/models/stock_quant.py
@@ -0,0 +1,25 @@
+from odoo import models
+
+
+class StockQuant(models.Model):
+    _inherit = "stock.quant"
+
+    def _get_quants_action(self, domain=None, extend=False):
+        action = super()._get_quants_action(domain=domain, extend=extend)
+        if self.env.user.has_group("stock.group_stock_manager"):
+            return action
+        if not self.env.user.has_group(
+            "stock_location_report_layout.group_stock_location_report_manager_layout_readonly"
+        ):
+            return action
+
+        readonly_view = self.env.ref(
+            "stock_location_report_layout.view_stock_quant_tree_readonly_manager_layout"
+        )
+        action["view_id"] = readonly_view.id
+        action["views"] = [(readonly_view.id, "list")] + [
+            (view_id, view_type)
+            for view_id, view_type in action.get("views", [])
+            if view_type != "list"
+        ]
+        return action
