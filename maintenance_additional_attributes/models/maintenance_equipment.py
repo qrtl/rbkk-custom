43c9bd8a6bdd668693dfb192b2423ad1f20e16b8
Morita Shinnosuke <morita@quartile.co>
2026-09-23 14:05:49 +0000

    Revert "Revert "fixup!""
    This reverts commit 001cd2f05db1e0edc0c9ff095cdc55e9eec163eb.

--- maintenance_additional_attributes/models/maintenance_equipment.py
+++ maintenance_additional_attributes/models/maintenance_equipment.py
@@ -18,7 +18,6 @@ class MaintenanceEquipment(models.Model):
     fixed_asset_code = fields.Char()
     is_gmp = fields.Boolean(string="GMP")
     management_no = fields.Char()
-    is_measuring_instrument = fields.Boolean(string="Measuring Instrument")
     is_national_project = fields.Boolean(string="National Project")
     national_project_name = fields.Char()
     location_id = fields.Many2one(
@@ -37,7 +36,6 @@ class MaintenanceEquipment(models.Model):
     )
     usable_temperature = fields.Char(string="Usable Temperature (°C)")
     usable_humidity = fields.Char(string="Usable Humidity (%RH, Non-condensing)")
-    is_daily_inspection = fields.Boolean(string="Daily Inspection")
-    is_calibration = fields.Boolean(string="Calibration")
+    is_manufacturer_calibration = fields.Boolean(string="Manufacturer Calibration")
+    is_in_house_calibration = fields.Boolean(string="In-house Calibration")
     is_fluorocarbon_inspection = fields.Boolean(string="Fluorocarbon Inspection Target")
-    is_handled_in_house = fields.Boolean(string="Handled In-house")
